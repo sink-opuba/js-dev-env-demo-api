a01b08c8604749cf6f62976e0e1923c9b160aef6
sink-opuba <osinkopere@yahoo.com>
2019-08-01 14:09:03 +0100

    create package-lock.json and fixed bug in index.js

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-var express =  require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", process.env.PORT || 5000);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get("/", function(request, response) {
+  response.send("Hello World!");
 });
 
-app.get('/users', function(req, res) {
+app.get("/users", function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Fiyin","lastName":"Soneye","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-    { "id": 3, "firstName": "Sinkopere", "lastName": "Opuba", "email": "osinkopere@yahoo.com" }
+    { id: 1, firstName: "Fiyin", lastName: "Soneye", email: "bob@gmail.com" },
+    {
+      id: 2,
+      firstName: "Tammy",
+      lastName: "Norton",
+      email: "tnorton@yahoo.com"
+    },
+    {
+      id: 3,
+      firstName: "Tina",
+      lastName: "Lee",
+      email: "lee.tina@hotmail.com"
+    },
+    {
+      id: 3,
+      firstName: "Sinkopere",
+      lastName: "Opuba",
+      email: "osinkopere@yahoo.com"
+    }
   ]);
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port')); 
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at localhost:" + app.get("port"));
 });
