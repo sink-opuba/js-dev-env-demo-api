4f367a32b7c9b46fe8ab604ed79a2e34c3873144
sink-opuba <osinkopere@yahoo.com>
2019-08-01 13:59:45 +0100

    update api

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ app.get('/', function(request, response) {
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 1,"firstName":"Fiyin","lastName":"Soneye","email":"bob@gmail.com"},
     {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
     {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    { "id": 3, "firstName": "Sinkopere", "lastName": "Opuba", "email": "osinkopere@yahoo.com" }
   ]);
 });
 
